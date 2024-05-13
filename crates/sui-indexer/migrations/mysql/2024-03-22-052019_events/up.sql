4b6ff7c60977901b6972c3ef289f83a14f66441b
Ge Gao <ge@mystenlabs.com>
2024-05-13 15:39:35 +0200

    tmp

--- crates/sui-indexer/migrations/mysql/2024-03-22-052019_events/up.sql
+++ crates/sui-indexer/migrations/mysql/2024-03-22-052019_events/up.sql
@@ -32,3 +32,4 @@ CREATE INDEX events_package_module ON events (package(255), module(255), tx_sequ
 CREATE INDEX events_event_type ON events (event_type(255), tx_sequence_number, event_sequence_number);
 CREATE INDEX events_type_package_module_name ON events (event_type_package(128), event_type_module(128), event_type_name(128), tx_sequence_number, event_sequence_number);
 CREATE INDEX events_checkpoint_sequence_number ON events (checkpoint_sequence_number);
+
